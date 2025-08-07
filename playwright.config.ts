61c68baffafe04a9f94ba916f8b3a6ce450ae86e
eugen-automation <doneugeniu@gmail.com>
2025-08-07 12:11:48 +0300

    Small code refactor

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,12 +24,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+ use: {
+    baseURL: 'https://demo.playwright.dev',
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
