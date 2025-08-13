a78cd32c00c88436f1bd5877d42cd351e85fe931
eugen-automation <doneugeniu@gmail.com>
2025-08-13 11:59:46 +0300

    Refactor header component

--- src/components/HeaderComponent.ts
+++ src/components/HeaderComponent.ts
@@ -2,18 +2,32 @@ import { Page, Locator } from '@playwright/test';
 
 export class HeaderComponent {
   readonly page: Page;
+  readonly registerLink: Locator;
+  readonly loginLink: Locator;
+  readonly wishListLink: Locator;
   readonly searchInput: Locator;
+  readonly searchBtn: Locator;
   readonly cartButton: Locator;
+  readonly logo: Locator;
+  readonly currencyDropDown: Locator;
+
+
 
   constructor(page: Page) {
     this.page = page;
-    this.searchInput = page.locator('input[name="q"]');
-    this.cartButton = page.locator('.cart-label');
+    this.registerLink = page.getByRole('link', { name: 'Register' });
+    this.loginLink = page.getByRole('link', { name: 'Log in' });
+    this.wishListLink = page.getByRole('link', { name: 'Wishlist (0)' })
+    this.searchInput = page.getByRole('textbox', { name: 'Search store' })
+    this.searchBtn = page.getByRole('button', { name: 'Search' })
+    this.cartButton = page.getByRole('link', { name: 'Shopping cart' })
+    this.logo = page.getByRole('link', { name: 'nopCommerce demo store' })
+    this.currencyDropDown = page.getByLabel('Currency selector');
   }
 
   async search(text: string) {
     await this.searchInput.fill(text);
-    await this.searchInput.press('Enter');
+    await this.searchBtn.click();
   }
 
   async openCart() {
